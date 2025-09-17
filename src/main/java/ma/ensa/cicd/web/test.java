cb51dd65425b826707c60923cd08fdb3a3bb616a
yassin99e <yassine.benakki@ump.ac.ma>
2025-09-17 22:30:54 +0100

    first commit

---
+++ src/main/java/ma/ensa/cicd/web/test.java
@@ -0,0 +1,24 @@
+package ma.ensa.cicd.web;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class test {
+
+
+
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+
+        String response = "Hello this is the First Edition";
+
+        return ResponseEntity.ok(response);
+    }
+
+}
